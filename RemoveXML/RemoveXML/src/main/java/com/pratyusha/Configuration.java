4e03f28cbc362e5ffed8677472ded4e10a4d0a56
Pratyusha Dhal <pratyusshaofficial@gmail.com>
2026-06-09 10:39:13 +0530

    Add files via upload

---
+++ RemoveXML/RemoveXML/src/main/java/com/pratyusha/Configuration.java
@@ -0,0 +1,8 @@
+package com.pratyusha;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@org.springframework.context.annotation.Configuration
+@ComponentScan(basePackages = "com.pratyusha")
+public class Configuration {
+}
---
+++ RemoveXML/RemoveXML/src/main/java/com/pratyusha/Entity/Student.java
@@ -0,0 +1,10 @@
+package com.pratyusha.Entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+    public void display(){
+        System.out.println("Object loaded...");
+    }
+}
